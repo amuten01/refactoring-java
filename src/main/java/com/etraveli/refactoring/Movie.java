eaf2ef67f2af529c5c6075adde839404b8b7971e
amuten01 <156481017+amuten01@users.noreply.github.com>
2024-02-17 23:59:50 +0100

    Removed duplicate code

--- src/main/java/com/etraveli/refactoring/Movie.java
+++ src/main/java/com/etraveli/refactoring/Movie.java
@@ -20,21 +20,3 @@ public class Movie {
         return code;
     }
 }
-
-public class Movie {
-    private String title;
-    private String code;
-
-    public Movie(String title, String code) {
-        this.title = title;
-        this.code = code;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getCode() {
-        return code;
-    }
-}
