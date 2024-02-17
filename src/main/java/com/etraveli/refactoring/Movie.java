2f720564cddc679cac1594701f528c7248abc494
amuten01 <156481017+amuten01@users.noreply.github.com>
2024-02-17 22:49:55 +0100

    Update Movie.java
    Added class comment

--- src/main/java/com/etraveli/refactoring/Movie.java
+++ src/main/java/com/etraveli/refactoring/Movie.java
@@ -1,5 +1,26 @@
 package com.etraveli.refactoring;
 
+/* This class encapsulates the information related to a movie, providing 
+a structured representation that can be used within the movie rental system 
+to identify and categorize movies available for rental. */
+public class Movie {
+    private String title;
+    private String code;
+
+    public Movie(String title, String code) {
+        this.title = title;
+        this.code = code;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCode() {
+        return code;
+    }
+}
+
 public class Movie {
     private String title;
     private String code;
